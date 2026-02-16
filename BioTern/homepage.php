576e2f4b4eef37402930b836a76a6a912036a545
FelixLuisMateo <mateo.felixluis@gmail.com>
2026-02-16 08:58:28 +0800

    try changing the homepage

--- BioTern/homepage.php
+++ BioTern/homepage.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="x-ua-compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>BioTern | Home</title>
+    <title>BioTern || Home</title>
     <link rel="shortcut icon" type="image/x-icon" href="assets/images/favicon.ico">
     <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
     <link rel="stylesheet" type="text/css" href="assets/vendors/css/vendors.min.css">
