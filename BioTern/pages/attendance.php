81f15965064dab43839c6b16192dfbc603d77d99
JomerDeGuzman <jomerrdeguzman@gmail.com>
2026-04-21 22:48:10 +0800

    fix

--- BioTern/pages/attendance.php
+++ BioTern/pages/attendance.php
@@ -1806,10 +1806,10 @@ echo $stats['total_count'] ?? 0; ?></span>
                 <div class="row">
                     <div class="col-lg-12">
 
-                        <div class="card stretch stretch-full attendance-table-card">
+                        <div class="card stretch stretch-full attendance-table-card app-data-card app-data-toolbar">
                             <div class="card-body p-0">
-                                <div class="table-responsive">
-                                    <table class="table table-hover" id="attendanceList">
+                                <div class="table-responsive app-data-table-wrap">
+                                    <table class="table table-hover app-data-table" id="attendanceList">
                                         <thead>
                                             <tr>
                                                 <th class="wd-30">
