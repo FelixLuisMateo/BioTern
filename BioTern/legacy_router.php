067b274308383337544e249736e0b6a8328756a0
JomerDeGuzman <jomerrdeguzman@gmail.com>
2026-05-12 17:14:42 +0800

    feat(certificates): add Certificate of Completion functionality and update navigation

--- BioTern/legacy_router.php
+++ BioTern/legacy_router.php
@@ -143,6 +143,7 @@ $map = [
   'document_dtr.php' => 'documents/document_dtr.php',
   'document_waiver.php' => 'documents/document_waiver.php',
   'document_parent_consent.php' => 'documents/document_parent_consent.php',
+  'document_certificate.php' => 'documents/document_certificate.php',
   'student-profile.php' => 'pages/student-profile.php',
   'student-dtr.php' => 'pages/student-dtr.php',
   'student-internal-dtr.php' => 'pages/student-internal-dtr.php',
